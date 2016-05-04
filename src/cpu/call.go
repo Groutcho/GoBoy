512aff2013e7fa5f17b78a82eb202c5c5d3d4852
Sébastien Guimmara <sguimmara@pobox.com>
2016-05-04 21:48:27 +0200

    instructions: implement call instructions

---
+++ src/cpu/call.go
@@ -0,0 +1,53 @@
+package cpu
+
+// call to %2, SP=SP-2, (SP)=PC, PC=%2
+func xCD_call() int {
+	addr := FetchOperand16()
+	Call(addr)
+
+	return 6
+}
+
+// conditional call if CY 
+func xDC_call() int {
+	addr := FetchOperand16()
+	if GetFlagCy() {
+		Call(addr)
+		return 6		
+	} else {
+		return 3
+	}
+}
+
+// conditional call if !CY 
+func xD4_call() int {
+	addr := FetchOperand16()
+	if !GetFlagCy() {
+		Call(addr)
+		return 6		
+	} else {
+		return 3
+	}
+}
+
+// conditional call if Z 
+func xC4_call() int {
+	addr := FetchOperand16()
+	if GetFlagZf() {
+		Call(addr)
+		return 6		
+	} else {
+		return 3
+	}
+}
+
+// conditional call if !Z 
+func xCC_call() int {
+	addr := FetchOperand16()
+	if !GetFlagZf() {
+		Call(addr)
+		return 6		
+	} else {
+		return 3
+	}
+}
---
+++ src/cpu/call_test.go
@@ -0,0 +1,21 @@
+package cpu
+
+import "testing"
+
+func TestxCD_call(t* testing.T) {
+	program := [...]byte{
+		0x45, // garbage
+		0x45, // garbage
+		0x45, // garbage
+		0x45, // garbage
+		0x45, // garbage
+		0x45, // garbage
+		0xCD, 0xFF, 0x15, // call 0xFF15
+	}
+
+	SetPC(0x0006)
+	ExecuteNext()
+	if GetPC() != 0xFF15 {
+		t.Errorf("TestxCD_call() failed: expected PC @ 0xFF15, got 0x%04x", GetPC())
+	}
+}
