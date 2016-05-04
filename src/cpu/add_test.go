9da9ce4156ab3636d9dd6ba180f90af3770feab0
Sébastien Guimmara <sguimmara@pobox.com>
2016-05-05 01:20:24 +0200

    add: add tests

---
+++ src/cpu/add_test.go
@@ -0,0 +1,99 @@
+package cpu
+
+import "testing"
+
+func testFlags(t* testing.T, Z bool, N bool, H bool, C bool) {
+	if GetFlagZf() != Z {
+		if Z {
+			t.Error("TestFlags() failed: expected Z set")
+		} else {
+			t.Error("TestFlags() failed: expected Z unset")			
+		}
+	}
+
+	if GetFlagN() != N {
+		if N {
+			t.Error("TestFlags() failed: expected N set")
+		} else {
+			t.Error("TestFlags() failed: expected N unset")			
+		}
+	}
+
+	if GetFlagH() != H {
+		if H {
+			t.Error("TestFlags() failed: expected H set")
+		} else {
+			t.Error("TestFlags() failed: expected H unset")			
+		}
+	}
+
+	if GetFlagCy() != C {
+		if C {
+			t.Error("TestFlags() failed: expected C set")
+		} else {
+			t.Error("TestFlags() failed: expected C unset")			
+		}
+	}
+}
+
+func Test80_add(t* testing.T) {
+	SetF(0x00)
+	SetA(0x05)
+	SetB(0x06)
+	x80_add()
+
+	if a:= GetA(); a != 0x05 + 0x06 {
+		t.Errorf("Test80_add() failed: expected A = 0x0A, got 0x%02x", a)
+	}
+
+	testFlags(t, false, false, false, false)
+
+	SetF(0x00)
+	SetA(0xFF)
+	SetB(0x05)
+	x80_add()
+
+	if a:= GetA(); a != 0x04 {
+		t.Errorf("Test80_add() failed: expected A = 0x04, got 0x%02x", a)
+	}
+
+	testFlags(t, false, false, true, true)
+
+	SetF(0x00)
+	SetA(0x00)
+	SetB(0x00)
+	x80_add()
+
+	if a:= GetA(); a != 0x00 {
+		t.Errorf("Test80_add() failed: expected A = 0x00, got 0x%02x", a)
+	}
+
+	testFlags(t, true, false, false, false)
+}
+
+// add  HL, DE - HL = HL+DE
+func Test19_add(t* testing.T) {
+	SetF(0x00)
+	SetHL(0x0FDE)
+	SetDE(0x21FF)
+	x19_add()
+
+	if hl:= GetHL(); hl != 0x31DD {
+		t.Errorf("Test19_add() failed: expected HL = 0x31DD, got 0x%04x", hl)
+	}
+
+	testFlags(t, false, false, true, false)
+
+	SetF(0x00)
+	SetHL(0xFFDE)
+	SetDE(0x21FF)
+	x19_add()
+
+	if hl:= GetHL(); hl != 0x21DD {
+		t.Errorf("Test19_add() failed: expected HL = 0x21DD, got 0x%04x", hl)
+	}
+
+	testFlags(t, false, false, true, true)
+}
+
+	
