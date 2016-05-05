6d3aefbab20eb96e24e836b373facac824341a91
Sébastien Guimmara <sguimmara@pobox.com>
2016-05-06 00:16:42 +0200

    dec: add tests

---
+++ src/cpu/dec_test.go
@@ -0,0 +1,15 @@
+package cpu
+
+import "testing"
+
+// D = D-1
+func Test15_dec(t* testing.T) {
+	SetD(0x15)
+	x15_dec()
+	testRegister(t, REG_D, 0x14)
+
+	SetD(0x00)
+	x15_dec()
+	testRegister(t, REG_D, 0xFF)
+	testFlags(t, false, true, true, false)
+}
