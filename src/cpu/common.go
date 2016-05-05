6c9c955a8ae53f208200bc5b57605c4cb5381838
Sébastien Guimmara <sguimmara@pobox.com>
2016-05-05 21:06:21 +0200

    common.go: add testRegister()

--- src/cpu/common.go
+++ src/cpu/common.go
@@ -2,6 +2,23 @@ package cpu
 
 import "testing"
 
+var (
+	REG_A = 0
+	REG_F = 1
+	REG_AF = 2
+	REG_B = 3
+	REG_C = 4
+	REG_BC = 5
+	REG_D = 6
+	REG_E = 7
+	REG_DE = 8
+	REG_H = 9
+	REG_L = 10
+	REG_HL = 11
+	REG_SP = 12
+	REG_PC = 13
+)
+
 func testFlags(t* testing.T, Z bool, N bool, H bool, C bool) {
 	if GetFlagZf() != Z {
 		if Z {
@@ -33,5 +50,32 @@ func testFlags(t* testing.T, Z bool, N bool, H bool, C bool) {
 		} else {
 			t.Error("TestFlags() failed: expected C unset")			
 		}
+	}
+}
+
+func testRegister(t* testing.T, registerCode int, expected int) {
+
+	name := ""
+	actual := 0
+
+	switch registerCode {
+		case REG_A: name = "A"; actual = int(GetA())
+		case REG_B: name = "B"; actual = int(GetB())
+		case REG_C: name = "C"; actual = int(GetC())
+		case REG_D: name = "D"; actual = int(GetD())
+		case REG_E: name = "E"; actual = int(GetE())
+		case REG_F: name = "F"; actual = int(GetF())
+		case REG_H: name = "H"; actual = int(GetH())
+		case REG_L: name = "L"; actual = int(GetL())
+		case REG_AF: name = "AF"; actual = int(GetAF())
+		case REG_BC: name = "BC"; actual = int(GetBC())
+		case REG_DE: name = "DE"; actual = int(GetDE())
+		case REG_HL: name = "HL"; actual = int(GetHL())
+		case REG_SP: name = "SP"; actual = int(GetSP())
+		case REG_PC: name = "PC"; actual = int(GetPC())
+	}
+
+	if actual != expected {
+		t.Errorf("Register test failed: expected %s @ 0x%04X, got 0x%04X", name, expected, actual)
 	}
 }
