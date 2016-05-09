d8c3a831cfb62dc5e82f891dc79bb2ed5234cee2
Sébastien Guimmara <sguimmara@pobox.com>
2016-05-09 21:19:58 +0200

    cpu: add StartTest() & add Initialize()

--- src/cpu/cpu.go
+++ src/cpu/cpu.go
@@ -3,12 +3,16 @@ package cpu
 import . "memory"
 import . "common"
 import t "time"
+import "log"
+import "os"
 
 // The dispatch table is used to redirect a given instruction to its
 // implementation by having a direct mapping between the opcode and the array index.
 // In the case of extended opcodes (CB XX), the mapping is the actual opcode (XX) + FF.
 // example: the instruction CB F8 will sit at index FF+F8 of the dispatch table.
 var dispatch_table []instrFunc = make([]instrFunc, 512, 512)
+
+var exitOnStop = false
 
 func push(value uint16) {
 	DecSP()
@@ -21,16 +25,18 @@ func push(value uint16) {
 // Get the opcode at the current PC, increment PC then return the opcode.
 // If opcode is an extended opcode, i.e CB XX, return FF + XX after
 // incrementing the PC twice.
-func Fetch() uint16 {
+func Fetch() (uint16, int) {
 	opcode := uint16(Get(GetPC()))
+	inc := 1
 	IncPC()
 
 	if opcode == 0xCB {
+		inc = 2
 		opcode = 0xFF + uint16(Get(GetPC()))
 		IncPC()
 	}
 
-	return opcode
+	return opcode, inc
 }
 
 // Get the 8bit word at the address pointed by the program counter
@@ -75,36 +81,78 @@ func Call(addr uint16) {
 	SetPC(addr)
 }
 
+func DumpRegisters() {
+	log.Printf("A: 0x%02X  F: 0x%02X\n", GetA(), GetF())
+}
+
 // execute the next instruction and return the number of cycles taken
 // by this instruction, as a multiple of 4, i.e unit cycles and not
 // actual CPU cycles. The minimal amount of cycles is 1.
 func ExecuteNext() int {
-	opcode := Fetch()
+	var opcode = uint16(0)
+	var inc = 0
+
+	defer func() {
+		if x := recover(); x != nil {
+			log.Printf("Fatal instruction: 0x%04X", GetPC() - uint16(inc))
+			os.Exit(1)
+		}
+	}()
+
+	opcode, inc = Fetch()
+	log.Printf("[0x%04X] %02X", GetPC() - uint16(inc), opcode)
 
 	if opcode == 0x76 { // halt
 		// TODO
 		return 1
 	} else if opcode == 0x10 { // stop
-		// TODO
-		return 0
+		if exitOnStop {
+			log.Print("Exiting upon STOP (exitOnStop true)")
+			DumpRegisters()
+			return 0
+		}
+		return 1
 	}
 
 	return dispatch_table[opcode]()
 }
 
+func Update(timescale int) int {
+	// execute the next instruction and get its execution time, in microseconds
+	wait_microsec := ExecuteNext()
+	if wait_microsec > 0 {
+		t.Sleep(t.Duration(wait_microsec * timescale) * t.Microsecond)
+		return 0
+	} else {
+		return -1
+	}
+}
+
 // Starts the execution of the program at any point
 func Start() {
-	wait_microsec := 0
-
 	for {
-		// execute the next instruction and get its execution time, in microseconds
-		wait_microsec = ExecuteNext()
-		if wait_microsec > 0 {
-			t.Sleep(t.Duration(wait_microsec) * t.Microsecond)
-		} else {
+		Update(1)
+	}
+}
+
+// Starts the execution of the program in test mode, executing at most
+// maxInstructions at normal speed.
+func StartTest(maxInstructions int) {
+	ExitOnStop(true)
+	ret := 0
+	for i := 0; i < maxInstructions; i++ {
+		ret = Update(1)
+		if ret == -1 {
 			return
 		}
 	}
+}
+
+// When encountering a STOP (0x10) opcode, tell the emulator
+// to exit the program and dump the registers. Useful for debug
+// purposes when executing custom test roms.
+func ExitOnStop(exit bool) {
+	exitOnStop = exit
 }
 
 // Load given program at address 0x0000
@@ -113,7 +161,44 @@ func LoadProgram(program []byte) {
 }
 
 func Initialize() {
-	Reset()
+	SetA(0x01)
+	SetF(0xB0)
+	SetBC(0x0013)
+	SetDE(0x00D8)
+	SetHL(0x014D)
+	SetSP(0xFFFE)
+	SetPC(0x100)
+
+	SetTIMA(0x00)
+	SetTMA(0x00)
+	SetTAC(0x00)
+	SetNR10(0x80)
+	SetNR11(0xBF)
+	SetNR12(0xF3)
+	SetNR14(0xBF)
+	SetNR21(0x3F)
+	SetNR24(0xBF)
+	SetNR30(0x7F)
+	SetNR31(0xFF)
+	// SetNR32(0x9F)
+	SetNR33(0xBF)
+	SetNR41(0xFF)
+	SetNR42(0x00)
+	// SetNR43(0x00)
+	// TODO: NR30 ??
+	SetNR50(0x77)
+	SetNR51(0xF3)
+	SetNR52(0xF1)
+	SetLCDC(0x91)
+	SetSCY(0x00)
+	SetSCX(0x00)
+	SetLYC(0x00)
+	SetBGP(0xFC)
+	SetOBP0(0xFF)
+	SetOBP1(0xFF)
+	SetWY(0x00)
+	SetWX(0x00)
+	SetWX(0x00)
 }
 
 type instrFunc func() int
